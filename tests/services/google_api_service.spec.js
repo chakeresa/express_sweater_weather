57788bcdae0a226e7376f7060b2e3f0f9ec4b064
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-16 23:53:41 -0600

    Passing test of geocoding

--- tests/services/google_api_service.spec.js
+++ tests/services/google_api_service.spec.js
@@ -17,7 +17,13 @@ describe('GoogleApiService', () => {
       let locationStr = 'denver,co'
       let service = new GoogleApiService(locationStr);
 
-      expect(service.geocodingResults()).toEqual("TODO: something");
+      return service.geocodingResults()
+        .then(response => {
+          result = response.results[0]
+          expect(result.formatted_address).toEqual("Denver, CO, USA");
+          expect(result.geometry.location.lat).toEqual(39.7392358);
+          expect(result.geometry.location.lng).toEqual(-104.990251);
+        })
     });
   });
 });
--- util/google_api_service.js
+++ util/google_api_service.js
@@ -5,17 +5,10 @@ class GoogleApiService {
     this.locationStr = locationStr;
   }
 
-  domain() {
-    return 'https://maps.googleapis.com';
-  }
-
   geocodingResults() {
-    return this.makeGetRequest()
-  }
-
-  makeGetRequest() {
+    let domain = 'https://maps.googleapis.com'
     var options = {
-      uri: this.domain() + '/maps/api/geocode/json',
+      uri: domain + '/maps/api/geocode/json',
       qs: {
         key: process.env.GOOGLE_MAPS_API_KEY,
         address: this.locationStr
@@ -23,46 +16,7 @@ class GoogleApiService {
       json: true
     };
 
-    let response;
-
-    rp(options)
-      .then(body => {
-        // body.results.formatted_address = "Denver, CO, USA"
-        // body.results.geometry.location.lat = 39.7392358
-        // body.results.geometry.location.lng = -104.990251
-        console.log(body);
-        response = body;
-        return body;
-        // TODO: fix -- not exporting as return value of geocodingResults
-      })
-      .catch(err => {
-        console.log('API call failed')
-      });
-
-    // return response;
-
-    // uri_path = '/maps/api/geocode/json'
-    // Rails.logger.debug "Making Google geocoding API call (#{@location_string})"
-    // location_hash = fetch_json_data(uri_path, address: @location_string)
-    // check_and_raise_error(location_hash)
-    // location_hash
-  }
-
-  conn() {
-    // @conn ||= Faraday.new(: url => 'https://maps.googleapis.com') do | faraday |
-    //   faraday.adapter Faraday.default_adapter
-    //   faraday.params['key'] = ENV['GOOGLE_MAPS_API_KEY']
-    // end
-  }
-
-  fetchJsonData(uri_path, params) {
-    // response = conn.get uri_path, params
-    // JSON.parse response.body, symbolize_names: true
-  }
-
-  checkAndRaiseError(response) {
-    // error_message = response[:error_message]
-    // raise "#{self.class} error: #{error_message}" if error_message
+    return rp(options)
   }
 }
 
