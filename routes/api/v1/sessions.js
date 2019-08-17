9308b189f49bbf358a2a5a6a342ddc54241c0aa4
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-16 20:47:47 -0600

    Fix sessionsRouter for case of missing inputs

--- routes/api/v1/sessions.js
+++ routes/api/v1/sessions.js
@@ -7,23 +7,31 @@ const bcrypt = require('bcrypt');
 /*POST new session (user login)*/
 router.post("/", function (req, res, next) {
   let passwordEntered = req.body.password;
-  User.findOne({
-    where: { email: req.body.email } 
-  }).then(user => {
-    if (user) {
-      authenticated = bcrypt.compareSync(passwordEntered, user.passwordDigest);
-      if (authenticated) {
-        res.setHeader("Content-Type", "application/json");
-        res.status(200).send(JSON.stringify({ api_key: user.apiKey }));
+
+  function unauthorized() {
+    res.setHeader("Content-Type", "application/json");
+    res.status(400).send(JSON.stringify({ error: "Invalid username or password" }));
+  }
+
+  if (!req.body.email || !passwordEntered) {
+    unauthorized();
+  } else {
+    User.findOne({
+      where: { email: req.body.email } 
+    }).then(user => {
+      if (user) {
+        authenticated = bcrypt.compareSync(passwordEntered, user.passwordDigest);
+        if (authenticated) {
+          res.setHeader("Content-Type", "application/json");
+          res.status(200).send(JSON.stringify({ api_key: user.apiKey }));
+        } else {
+          unauthorized();
+        }
       } else {
-        res.setHeader("Content-Type", "application/json");
-        res.status(400).send(JSON.stringify({ error: "Invalid username or password" }));
+        unauthorized();
       }
-    } else {
-      res.setHeader("Content-Type", "application/json");
-      res.status(400).send(JSON.stringify({ error: "Invalid username or password" }));
-    }
-  })
+    })
+  }
 });
 
 module.exports = router;
