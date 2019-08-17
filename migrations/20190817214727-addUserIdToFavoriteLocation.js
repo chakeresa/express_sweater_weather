1045c363424b27acd051299d65241ed9b1a016bb
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-17 16:00:16 -0600

    Add UserId to FavoriteLocations (with migration)

---
+++ migrations/20190817214727-addUserIdToFavoriteLocation.js
@@ -0,0 +1,26 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'FavoriteLocations',
+      'UserId',
+      {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      }
+    );
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn(
+      'FavoriteLocations',
+      'UserId'
+    )
+  }
+};
