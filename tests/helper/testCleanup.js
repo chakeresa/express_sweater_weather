6dc15e918b8a1ebf433c164ba3dbc833a5e3f5ff
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-17 20:58:09 -0600

    Refactor testCleanup

--- tests/helper/testCleanup.js
+++ tests/helper/testCleanup.js
@@ -2,6 +2,8 @@ var User = require('../../models').User;
 var FavoriteLocation = require('../../models').FavoriteLocation;
 
 module.exports = async function cleanup() {
-  await User.destroy({ where: {} })
-  await FavoriteLocation.destroy({ where: {} })
+  return User.destroy({ where: {} })
+    .then(user => {
+      FavoriteLocation.destroy({ where: {} })
+    })
 }
