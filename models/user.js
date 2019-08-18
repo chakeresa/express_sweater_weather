fa9f0b204d73c0e321327104e97391bf33643c68
Alexandra Chakeres <alexchakeres@gmail.com>
2019-08-18 13:57:54 -0600

    Add unique constraint to email attribute of User

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    email: DataTypes.STRING,
+    email: {type: DataTypes.STRING, unique: true },
     passwordDigest: DataTypes.STRING,
     apiKey: DataTypes.STRING
   }, {});
